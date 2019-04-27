52d69d483f63233743a0ae2f8bebc2d9c546319f
Kevin <kevinohagan@hotmail.co.uk>
2019-04-27 22:24:59 +0100

    1st C , Selectors need fixed, save button displayed under new list items, no border around form

---
+++ js/app.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+  const form = document.querySelector('#winner-list');
+
+
+
+  const formSubmitHandler = function (event) {
+
+    //Prevent page refresh
+    event.preventDefault();
+        console.log('button clicked');
+    // Getting playerName text input value
+    const playerNameValue =  event.target.playerName.value;
+    // Getting nationality text input value
+    const nationalityValue =  event.target.nationality.value;
+    //Getting selector value
+    const positionValue = event.target.position.value;
+    //Getting dropdown value
+    const yearWonValue = event.target.year.value;
+
+
+    //CREATING DIV CONTAINER
+    const divContainer = document.createElement('div');
+    const winnerList = document.querySelector('#winner-list');
+    //ADDING DIV TO WINNER LIST
+    winnerList.appendChild(divContainer);
+    //CREATE P ELEMENT FOR NAME
+    const pPlayerName = document.createElement('p');
+    //SET title value to title input
+    pPlayerName.textContent = playerNameValue;
+    //Attach to divContainer
+    divContainer.appendChild(pPlayerName);
+
+
+    const pNationality = document.createElement('p');
+    pNationality.textContent = nationalityValue;
+    divContainer.appendChild(pNationality);
+
+    const pPosition = document.createElement('p');
+    pPosition.textContent = positionValue;
+    divContainer.appendChild(pPosition);
+
+    const pYear = document.createElement('p');
+    pYear.textContent = yearWonValue;
+    divContainer.appendChild(pYear);
+
+  }
+
+
+  form.addEventListener('submit', formSubmitHandler);
+})
